de677c434d2f6d3fd311c7e0ac110a72e25171b7
Matheus Muruci <matheusmuruci@outlook.com>
2026-03-03 17:22:51 -0300

    feat[Categorias]: Criado componente de categorias e demonstrado na página inicial

---
+++ src/app/index/index.tsx
@@ -0,0 +1,22 @@
+import { View, Image, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+import { Categories } from "@/components/categories";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/app/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity activeOpacity={0.3}>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+
+      <Categories />
+    </View>
+  );
+}
