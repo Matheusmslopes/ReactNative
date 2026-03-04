75945cb33900e29b6532db745ca9765741376d35
Matheus Muruci <matheusmuruci@outlook.com>
2026-03-04 15:18:36 -0300

    feat: Adicionado componentes de botão e input, e uma tela para adicionar um link

---
+++ src/app/add/index.tsx
@@ -0,0 +1,55 @@
+import { Alert, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
+
+import { styles } from "./style";
+import { colors } from "@/styles/colors";
+import { router } from "expo-router";
+import { Categories } from "@/components/categories";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+export default function Add() {
+  const [category, setCategory] = useState("");
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+
+  function handleAdd() {
+    if (!category) {
+      console.log("Sem categoria selecionada");
+      //return Alert.alert("Categoria", "Selecione a categoria");
+    }
+
+    if (!name.trim()) {
+      console.log("Informe um nome");
+      //return Alert.alert("Nome", "Informe o nome");
+    }
+
+    if (!url.trim()) {
+      console.log("Informe uma url");
+      //return Alert.alert("URL", "Informe uma URL");
+    }
+    console.log({ category, name, url });
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Novo</Text>
+      </View>
+      <Text style={styles.label}>Selecione uma categoria</Text>
+
+      <Categories onChange={setCategory} selected={category} />
+
+      <View style={styles.form}>
+        <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
+        <Button title="Adicionar" onPress={handleAdd} />
+      </View>
+    </View>
+  );
+}
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -7,19 +7,24 @@ import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
+import { router } from "expo-router";
+import { useState } from "react";
+import { categories } from "@/utils/categories";
 
 export default function Index() {
+  const [category, setCategory] = useState(categories[0].name);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={require("@/app/assets/logo.png")} style={styles.logo} />
 
-        <TouchableOpacity activeOpacity={0.3}>
+        <TouchableOpacity onPress={() => router.navigate("./add")} activeOpacity={0.3}>
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
 
-      <Categories />
+      <Categories onChange={setCategory} selected={category} />
 
       <FlatList
         data={["1", "2", "3"]}
@@ -36,7 +41,7 @@ export default function Index() {
         showsHorizontalScrollIndicator={false}
       />
 
-      <Modal transparent visible={true}>
+      <Modal transparent visible={false}>
         <View style={styles.modal}>
           <View style={styles.modalContent}>
             <View style={styles.modalHeader}>
